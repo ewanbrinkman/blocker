75052473b0fc7741a054328d91dc8c4ced62667a
ewanbrinkman <ewanbrinkman@gmail.com>
2021-06-01 23:06:47 -0700

    Fix scrolling background scene gap based on speed

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -7,36 +7,92 @@ export default class TitleScene extends Phaser.Scene {
     }
 
     addBackground() {
+        console.log('Adding background.');
+
         // Get the screen size of the game.
         const height = this.cameras.main.height;
+        
+        let startX, tweenDuration, beforeBackground;
 
-        let beforeBackground = this.backgrounds[0];
+        if (this.backgrounds.length === 0) {
+            startX = 0;
+        } else {
+            beforeBackground = this.backgrounds[this.backgrounds.length - 1];
+            startX = beforeBackground.getTopRight().x - (0.0336 * TITLE_SCENE.backgroundSpeed);
+        }
 
-        console.log(beforeBackground.getTopRight().x, beforeBackground.displayWidth);
+        let backgroundNumber = Phaser.Math.Between(1, 2).toString();
+        let background = this.add.image(startX, height, 'backgroundTitle' + backgroundNumber);
+        background.setOrigin(0, 1);
+        // Place the background image behind everything else.
+        background.setDepth(-1);
 
-        let newBackground = this.add.image(beforeBackground.displayWidth, height, 'backgroundTitle2');
-        newBackground.setOrigin(0, 1);
+        if (this.backgrounds.length === 0) {
+            tweenDuration = (background.displayWidth / TITLE_SCENE.backgroundSpeed) * 1000;
+        } else {
+            tweenDuration = ((background.displayWidth + beforeBackground.getTopRight().x) / TITLE_SCENE.backgroundSpeed) * 1000;
 
-        let tweenDuration = ((newBackground.displayWidth + beforeBackground.displayWidth) / TITLE_SCENE.backgroundSpeed) * 1000;
-        this.tweens.add({
-            targets: newBackground,
-            x: -newBackground.displayWidth,
+            // Make sure the background image is connected correctly after a bit of time.
+            // this.time.delayedCall(200, () => {
+            //     background.setX(beforeBackground.getTopRight().x);
+            //     console.log('delayed', background.x, beforeBackground.getTopRight().x);
+            // }, [], this);
+
+            // this.time.delayedCall(300, () => {
+            //     console.log('delayed 2', background.x, beforeBackground.getTopRight().x);
+            // }, [], this);
+        }
+
+        let backgroundTween = this.tweens.add({
+            targets: background,
+            x: -background.displayWidth,
             ease: Phaser.Math.Easing.Linear,
             // Time = distance / speed. Multiply by 1000 to go from seconds to milliseconds.
             duration: tweenDuration,
+            // speed: TITLE_SCENE.backgroundSpeed,
+            // completeDelay: 1000,
             onComplete: () => {
                 this.backgrounds.shift();
-                newBackground.destroy();
+                background.destroy();
 
                 // Add another background to the scrolling backgrounds.
                 this.addBackground();
             },
         });
-        this.backgrounds.push(newBackground);
 
-        console.log('duration', tweenDuration);
+        // if (this.backgrounds.length !== 0) {
+        //     this.time.delayedCall(200, () => {
+        //         console.log(backgroundTween.elapsed);
+        //         backgroundTween.elapsed += (background.getTopLeft().x - beforeBackground.getTopRight().x) / TITLE_SCENE.backgroundSpeed;
+        //         console.log(backgroundTween.elapsed);
+        //     }, [], this);
+        // }
 
-        console.log(beforeBackground.getTopRight().x, beforeBackground.displayWidth);
+        this.backgrounds.push(background);
+
+
+
+        // let beforeBackground = this.backgrounds[0];
+
+        // let newBackground = this.add.image(beforeBackground.displayWidth, height, 'backgroundTitle2');
+        // newBackground.setOrigin(0, 1);
+
+        // let tweenDuration = ((newBackground.displayWidth + beforeBackground.displayWidth) / TITLE_SCENE.backgroundSpeed) * 1000;
+        // this.tweens.add({
+        //     targets: newBackground,
+        //     x: -newBackground.displayWidth,
+        //     ease: Phaser.Math.Easing.Linear,
+        //     // Time = distance / speed. Multiply by 1000 to go from seconds to milliseconds.
+        //     duration: tweenDuration,
+        //     onComplete: () => {
+        //         this.backgrounds.shift();
+        //         newBackground.destroy();
+
+        //         // Add another background to the scrolling backgrounds.
+        //         this.addBackground();
+        //     },
+        // });
+        // this.backgrounds.push(newBackground);
     }
 
     create() {
@@ -47,6 +103,12 @@ export default class TitleScene extends Phaser.Scene {
         this.events.on('shutdown', () => {
             this.scale.off('resize', this.resize);
         });
+
+        this.input.keyboard.on('keydown-B', () => {
+            let x1right = this.backgrounds[0].getTopRight().x;
+            let x2left = this.backgrounds[1].getTopLeft().x;
+            console.log('Space Between:', x2left - x1right);
+        })
 
         // Get the screen size of the game.
         const width = this.cameras.main.width;
@@ -60,31 +122,31 @@ export default class TitleScene extends Phaser.Scene {
         // this.background.setOrigin(0.5, 1);
 
         // The background image of world tiles.
-        this.backgrounds = []
-        let startBackground = this.add.image(0, height, 'backgroundTitle1');
-        startBackground.setOrigin(0, 1);
+        // this.backgrounds = []
+        // let startBackground = this.add.image(0, height, 'backgroundTitle1');
+        // startBackground.setOrigin(0, 1);
 
-        let tweenDuration = (startBackground.displayWidth / TITLE_SCENE.backgroundSpeed) * 1000;
-        this.tweens.add({
-            targets: startBackground,
-            x: -startBackground.displayWidth,
-            ease: Phaser.Math.Easing.Linear,
-            // Time = distance / speed. Multiply by 1000 to go from seconds to milliseconds.
-            duration: tweenDuration,
-            onComplete: () => {
-                // Remove the finished background.
-                this.backgrounds.shift();
-                startBackground.destroy();
+        // let tweenDuration = (startBackground.displayWidth / TITLE_SCENE.backgroundSpeed) * 1000;
+        // this.tweens.add({
+        //     targets: startBackground,
+        //     x: -startBackground.displayWidth,
+        //     ease: Phaser.Math.Easing.Linear,
+        //     // Time = distance / speed. Multiply by 1000 to go from seconds to milliseconds.
+        //     duration: tweenDuration,
+        //     onComplete: () => {
+        //         // Remove the finished background.
+        //         this.backgrounds.shift();
+        //         startBackground.destroy();
 
-                // Add another background to the scrolling backgrounds.
-                this.addBackground();
-            },
-        });
-        this.backgrounds.push(startBackground);
-
-        console.log('duration', tweenDuration);
+        //         // Add another background to the scrolling backgrounds.
+        //         this.addBackground();
+        //     },
+        // });
+        // this.backgrounds.push(startBackground);
 
         // Add another background to the scrolling backgrounds.
+        this.backgrounds = []
+        this.addBackground();
         this.addBackground();
 
         // Add the logo image.
@@ -126,6 +188,14 @@ export default class TitleScene extends Phaser.Scene {
             'greenbuttondown',
             'Credits',
             SCENE_KEYS.credits);
+
+        this.keyB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
+    }
+
+    update(time, delta) {
+        if (Phaser.Input.Keyboard.JustDown(this.keyB)) {
+            console.log('Delta:', delta);
+        }
     }
 
     resize() {
