2dae1c24348eb09ea409f7faf03e7455f9e7728c
ewanbrinkman <ewanbrinkman@gmail.com>
2021-06-14 14:42:37 -0700

    Add splash text

--- src/constants/splashes.js
+++ src/constants/splashes.js
@@ -6,5 +6,7 @@ export const SPLASHES = [
     'print("Hello, Blocker!")',
     'Should it be called squarer?',
     'The derivative of e^2 is not 2e!',
-    'Infinity over infinity? More work required!'
+    'Infinity over infinity? More work required!',
+    'So many shortcuts!',
+    'Are cats a liquid?'
 ]
